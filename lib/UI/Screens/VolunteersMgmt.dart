f1f9309495d079da67fa340884e400e380aea1ce
Ahmed Saad <ahmed@Ahmeds-MacBook-Pro.local>
2025-12-20 15:19:41 +0300

    Pre-edit checkpoint: Implementing Volunteers Management screen

---
+++ lib/UI/Screens/VolunteersMgmt.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VolunteersMgmt extends StatelessWidget {
+  const VolunteersMgmt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
